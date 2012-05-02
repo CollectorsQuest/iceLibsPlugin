7fe027770994b8f9c776ca36666a84b8a2ed928f
Yanko Simeonoff <yanko@zecho.info>
2012-05-02 18:02:45 +0300

    Removed as moved to iceSEOPlugin

--- modules/iceGlobalModule/lib/BaseiceGlobalModuleComponents.class.php
+++ modules/iceGlobalModule/lib/BaseiceGlobalModuleComponents.class.php
@@ -2,13 +2,4 @@
 
 class BaseiceGlobalModuleComponents extends sfComponents
 {
-  public function executeBreadcrumbs()
-  {
-    $module = $this->getContext()->getModuleName();
-    $action = $this->getContext()->getActionName();
-
-    $ice_propel_breadcrumbs = new IcePropelBreadcrumbs($module, $action);
-    $this->breadcrumbs = $ice_propel_breadcrumbs->getBreadcrumbs();
-    $this->separator = $ice_propel_breadcrumbs->getSeparator();
-  }
 }
