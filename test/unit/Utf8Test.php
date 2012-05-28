0238bfc66b4a0260872e9550560f3751ddba46f0
Yanko Simeonoff <yanko@zecho.info>
2012-05-28 11:51:06 +0300

    Added on slugify conversion of '&' to 'and'

--- test/unit/Utf8Test.php
+++ test/unit/Utf8Test.php
@@ -154,6 +154,9 @@ $t->diag('::slugify()');
   $slug = Utf8::slugify('????', '-', true, true, 'default');
   $t->is($slug, 'default', 'Checking the default parameter');
 
+  $slug = Utf8::slugify('History & Militaria', '-', true, true);
+  $t->is($slug, 'history-and-militaria', 'Ampersand should become "and"');
+
 $t->diag('::excerpt()');
 
   $texts = array(
