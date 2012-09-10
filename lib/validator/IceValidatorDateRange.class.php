022a42c903714370e6e4db6b7149b60e7168b3ca
Kiril Angov <kiril.angov@gmail.com>
2012-09-10 22:40:48 +0900

    Fixing a problem with IceValidatorDateRange and single dates

--- lib/validator/IceValidatorDateRange.class.php
+++ lib/validator/IceValidatorDateRange.class.php
@@ -14,14 +14,18 @@ class IceValidatorDateRange extends sfValidatorDateRange
     {
       @list($from, $to) = explode(' - ', $value);
 
-      $value = array(
-        $fromField => !empty($from) ? date_format(new DateTime($from), 'm/d/Y') : null,
-        $toField => !empty($to) ? date_format(new DateTime($to), 'm/d/Y') : null
-      );
+      $value = array();
+      $value[$fromField] = !empty($from) ? date_format(new DateTime($from), 'm/d/Y') : null;
+      $value[$toField] = !empty($to) ? date_format(new DateTime($to), 'm/d/Y') : $value[$fromField];
     }
 
     if ($value[$fromField] && $value[$toField])
     {
+      if ($value[$fromField] === $value[$toField] && $value[$toField] !== null)
+      {
+        $value[$toField] = date('m/d/Y', strtotime('+1 day', strtotime($value[$toField])));
+      }
+
       $v = new sfValidatorSchemaCompare(
         $fromField, sfValidatorSchemaCompare::LESS_THAN_EQUAL, $toField,
         array('throw_global_error' => true), array('invalid' => $this->getMessage('invalid'))
